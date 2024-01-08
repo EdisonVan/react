65503663e3bf1fffd4b4046a1e07107a979cbc7e
Andrew Clark <git@andrewclark.io>
2024-01-07 23:17:20 -0500

    Fix: useDeferredValue initialValue suspends forever without switching to final (#27888)
    Fixes a bug in the experimental `initialValue` option for
    `useDeferredValue` (added in #27500).
    
    If rendering the `initialValue` causes the tree to suspend, React should
    skip it and switch to rendering the final value instead. It should not
    wait for `initialValue` to resolve.
    
    This is not just an optimization, because in some cases the initial
    value may _never_ resolve — intentionally. For example, if the
    application does not provide an instant fallback state. This capability
    is, in fact, the primary motivation for the `initialValue` API.
    
    I mostly implemented this correctly in the original PR, but I missed
    some cases where it wasn't working:
    
    - If there's no Suspense boundary between the `useDeferredValue` hook
    and the component that suspends, and we're not in the shell of the
    transition (i.e. there's a parent Suspense boundary wrapping the
    `useDeferredValue` hook), the deferred task would get incorrectly
    dropped.
    - Similarly, if there's no Suspense boundary between the
    `useDeferredValue` hook and the component that suspends, and we're
    rendering a synchronous update, the deferred task would get incorrectly
    dropped.
    
    What these cases have in common is that it causes the `useDeferredValue`
    hook itself to be replaced by a Suspense fallback. The fix was the same
    for both. (It already worked in cases where there's no Suspense fallback
    at all, because those are handled differently, at the root.)
    
    The way I discovered this was when investigating a particular bug in
    Next.js that would happen during a 'popstate' transition (back/forward),
    but not during a regular navigation. That's because we render popstate
    transitions synchronously to preserve browser's scroll position — which
    in this case triggered the second scenario above.

--- packages/react-reconciler/src/ReactFiberBeginWork.js
+++ packages/react-reconciler/src/ReactFiberBeginWork.js
@@ -90,6 +90,7 @@ import {
   ShouldCapture,
   ForceClientRender,
   Passive,
+  DidDefer,
 } from './ReactFiberFlags';
 import ReactSharedInternals from 'shared/ReactSharedInternals';
 import {
@@ -257,6 +258,7 @@ import {
   renderDidSuspendDelayIfPossible,
   markSkippedUpdateLanes,
   getWorkInProgressRoot,
+  peekDeferredLane,
 } from './ReactFiberWorkLoop';
 import {enqueueConcurrentRenderForLane} from './ReactFiberConcurrentUpdates';
 import {pushCacheProvider, CacheContext} from './ReactFiberCacheComponent';
@@ -2229,9 +2231,22 @@ function shouldRemainOnFallback(
   );
 }
 
-function getRemainingWorkInPrimaryTree(current: Fiber, renderLanes: Lanes) {
-  // TODO: Should not remove render lanes that were pinged during this render
-  return removeLanes(current.childLanes, renderLanes);
+function getRemainingWorkInPrimaryTree(
+  current: Fiber | null,
+  primaryTreeDidDefer: boolean,
+  renderLanes: Lanes,
+) {
+  let remainingLanes =
+    current !== null ? removeLanes(current.childLanes, renderLanes) : NoLanes;
+  if (primaryTreeDidDefer) {
+    // A useDeferredValue hook spawned a deferred task inside the primary tree.
+    // Ensure that we retry this component at the deferred priority.
+    // TODO: We could make this a per-subtree value instead of a global one.
+    // Would need to track it on the context stack somehow, similar to what
+    // we'd have to do for resumable contexts.
+    remainingLanes = mergeLanes(remainingLanes, peekDeferredLane());
+  }
+  return remainingLanes;
 }
 
 function updateSuspenseComponent(
@@ -2259,6 +2274,11 @@ function updateSuspenseComponent(
     showFallback = true;
     workInProgress.flags &= ~DidCapture;
   }
+
+  // Check if the primary children spawned a deferred task (useDeferredValue)
+  // during the first pass.
+  const didPrimaryChildrenDefer = (workInProgress.flags & DidDefer) !== NoFlags;
+  workInProgress.flags &= ~DidDefer;
 
   // OK, the next part is confusing. We're about to reconcile the Suspense
   // boundary's children. This involves some custom reconciliation logic. Two
@@ -2330,6 +2350,11 @@ function updateSuspenseComponent(
       const primaryChildFragment: Fiber = (workInProgress.child: any);
       primaryChildFragment.memoizedState =
         mountSuspenseOffscreenState(renderLanes);
+      primaryChildFragment.childLanes = getRemainingWorkInPrimaryTree(
+        current,
+        didPrimaryChildrenDefer,
+        renderLanes,
+      );
       workInProgress.memoizedState = SUSPENDED_MARKER;
       if (enableTransitionTracing) {
         const currentTransitions = getPendingTransitions();
@@ -2369,6 +2394,11 @@ function updateSuspenseComponent(
       const primaryChildFragment: Fiber = (workInProgress.child: any);
       primaryChildFragment.memoizedState =
         mountSuspenseOffscreenState(renderLanes);
+      primaryChildFragment.childLanes = getRemainingWorkInPrimaryTree(
+        current,
+        didPrimaryChildrenDefer,
+        renderLanes,
+      );
       workInProgress.memoizedState = SUSPENDED_MARKER;
 
       // TODO: Transition Tracing is not yet implemented for CPU Suspense.
@@ -2403,6 +2433,7 @@ function updateSuspenseComponent(
           current,
           workInProgress,
           didSuspend,
+          didPrimaryChildrenDefer,
           nextProps,
           dehydrated,
           prevState,
@@ -2465,6 +2496,7 @@ function updateSuspenseComponent(
       }
       primaryChildFragment.childLanes = getRemainingWorkInPrimaryTree(
         current,
+        didPrimaryChildrenDefer,
         renderLanes,
       );
       workInProgress.memoizedState = SUSPENDED_MARKER;
@@ -2835,6 +2867,7 @@ function updateDehydratedSuspenseComponent(
   current: Fiber,
   workInProgress: Fiber,
   didSuspend: boolean,
+  didPrimaryChildrenDefer: boolean,
   nextProps: any,
   suspenseInstance: SuspenseInstance,
   suspenseState: SuspenseState,
@@ -3064,6 +3097,11 @@ function updateDehydratedSuspenseComponent(
       const primaryChildFragment: Fiber = (workInProgress.child: any);
       primaryChildFragment.memoizedState =
         mountSuspenseOffscreenState(renderLanes);
+      primaryChildFragment.childLanes = getRemainingWorkInPrimaryTree(
+        current,
+        didPrimaryChildrenDefer,
+        renderLanes,
+      );
       workInProgress.memoizedState = SUSPENDED_MARKER;
       return fallbackChildFragment;
     }
